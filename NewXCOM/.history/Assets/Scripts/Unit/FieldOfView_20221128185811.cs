570fe147e1d37436e53f64e2589e6108fbbf9f60
VnssJT <64807616+VnssJT@users.noreply.github.com>
2022-11-30 15:52:02 +0100

    Revert "Revert "more stuff (sorry for shitty commit names)""
    This reverts commit 443e657f9055773a16dd759299f7ead8e9560461.

---
+++ NewXCOM/.history/Assets/Scripts/Unit/FieldOfView_20221128185811.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
---
+++ NewXCOM/.history/Assets/Scripts/Unit/FieldOfView_20221128194706.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    [SerializeField] private LayerMask layerMask;
+    private Mesh mesh;
+    private Vector3 origin;
+    private float startingAngle;
+
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+
+    private void Update() {
+        float fov = 90f;
+        origin = Vector3.zero;
+        int rayCount = 2;
+        float angle = 0f;
+        float angleIncrease = fov / rayCount;
+        float viewDistance = 50f;
+
+        Vector3[] vertices = new Vector3[rayCount + 1 + 1];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        vertices[0] = origin;
+
+
+        int vertexIndex = 1;
+        int triangleIndex = 0;
+        for(int i = 0; i < rayCount; i++)
+        {
+            Vector3 vertex;
+
+            // Detect walls 
+            RaycastHit2D raycastHit2D = Physics2D.Raycast(origin, GetVectorFromAngle(angle), viewDistance, layerMask);
+
+            if(raycastHit2D.collider == null)
+            {
+                // No hit
+                vertex = origin + GetVectorFromAngle(angle) * viewDistance;
+            }
+            else
+            {
+                // Hit object
+                vertex = raycastHit2D.point;
+            }
+
+            vertices[vertexIndex] = vertex;
+
+            if(i > 0)
+            {
+                triangles[triangleIndex + 0] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+
+                triangleIndex += 3;
+            }
+
+            vertexIndex++;
+
+            angle -= angleIncrease;
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+    }
+
+    // Copied from CodeMonkey utilities
+    public static Vector3 GetVectorFromAngle(float angle)
+    {
+        // angle = 0 -> 360
+        float angleRad = angle * (Mathf.PI/180f);
+        return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+    }
+
+    public void SetOrigin(Vector3 origin)
+    {
+        this.origin = origin;
+    }
+
+    public void SetAimDirection(Vector3 aimDirection)
+    {
+        startingAngle = 
+    }
+       
+}
