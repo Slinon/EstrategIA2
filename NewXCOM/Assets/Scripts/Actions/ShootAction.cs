5dd8a2ee547ac13af8b1fdec8fbb4b1a2b66aec7
Totewa <90398319+Totewa@users.noreply.github.com>
2022-12-12 01:17:44 +0100

    Merge branch 'main' into CoberturaToni

--- NewXCOM/Assets/Scripts/Actions/ShootAction.cs
+++ NewXCOM/Assets/Scripts/Actions/ShootAction.cs
@@ -275,7 +275,21 @@ public class ShootAction : BaseAction
 
                 // Calculamos la direccion de disparo
                 Vector3 unitWorldPosition = LevelGrid.Instance.GetWorldPosition(unitGridPosition);
+                Unit unitPosition = LevelGrid.Instance.GetUnitAtGridPosition(testGridPosition);
                 Vector3 shootDirection = (targetUnit.GetWorldPosition() - unitWorldPosition).normalized;
+
+                // Definimos un offset para poder disparar por encima de obstaculos bajos
+
+                
+
+
+                if(targetUnit.GetCoverType() == CoverType.Covered)
+                {
+                    if(unitPosition.GetCoverType() == CoverType.Covered)
+                    {
+                        unitShoulderHeight = 0.6f;
+                    }else{unitShoulderHeight = 1.7f;}         
+                }
 
                 // Comprobamos si la unidad no tiene visual del objetivo
                 if (Physics.Raycast(unitWorldPosition + Vector3.up * unitShoulderHeight, shootDirection,
