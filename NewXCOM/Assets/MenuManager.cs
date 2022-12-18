f3ada4825a8c250165bc612d89774d0f09aa6378
GoragarXGameDev <goragarxgamedev@gmail.com>
2022-12-18 17:39:07 +0100

    MainMenu

---
+++ NewXCOM/Assets/MenuManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MenuManager : MonoBehaviour
+{
+    public static string seed;
+    [SerializeField] private TMP_InputField inputField;
+
+    //Quit application
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    //load scene by name
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    //Get input field value and set it as seed
+    public void SetSeed()
+    {
+        seed = inputField.text;
+    }
+}
